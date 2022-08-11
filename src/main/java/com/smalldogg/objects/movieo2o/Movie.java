adc5ad9968dee5c417af57eeadbaccb851318a9a
박선용 <74956749+small-dogg@users.noreply.github.com>
2022-08-12 08:27:10 +0900

    feat: 상속을 통해, 할인정책에 대한 유연성 확보

--- src/main/java/com/smalldogg/objects/movieo2o/Movie.java
+++ src/main/java/com/smalldogg/objects/movieo2o/Movie.java
@@ -1,11 +1,17 @@
 package com.smalldogg.objects.movieo2o;
 
+import java.time.Duration;
+
 public class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
     private DiscountPolicy discountPolicy;
 
+    /**상속을 통해, DiscountPolicy는 정률과 정액 할인기준이 있는데, Movie 클래스만 보면 둘중 어떤 것을 사용하는지까지 알 수 없다.
+     * 대신, 확장성과 유연성이 확보되어있어, 정률 정액 할인기준 뿐만아니라 다른 정책이 신규로 들어오더라도 간편히 확장이 가능하고,
+     * 또한 유연하게 정률 정액 할인기준을 선택할 수 있는 장점이 있다.
+     */
     public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
         this.title = title;
         this.runningTime = runningTime;
