80241c85aabab96fbc249513e759f8ca8219d4c1
박선용 <74956749+small-dogg@users.noreply.github.com>
2022-08-14 16:24:30 +0900

    feat: 합성(Composite) 적용하기

--- src/main/java/com/smalldogg/objects/movieo2o/Movie.java
+++ src/main/java/com/smalldogg/objects/movieo2o/Movie.java
@@ -6,17 +6,20 @@ public class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
-    private DefaultDiscountPolicy defaultDiscountPolicy;
+    private DiscountPolicy discountPolicy;
 
     /**상속을 통해, DiscountPolicy는 정률과 정액 할인기준이 있는데, Movie 클래스만 보면 둘중 어떤 것을 사용하는지까지 알 수 없다.
      * 대신, 확장성과 유연성이 확보되어있어, 정률 정액 할인기준 뿐만아니라 다른 정책이 신규로 들어오더라도 간편히 확장이 가능하고,
      * 또한 유연하게 정률 정액 할인기준을 선택할 수 있는 장점이 있다.
      */
-    public Movie(String title, Duration runningTime, Money fee, DefaultDiscountPolicy defaultDiscountPolicy) {
+    /**
+     * Movie와 DiscountPolicy는 합성관계로 연결되어있다.
+     */
+    public Movie(String title, Duration runningTime, Money fee, DefaultDiscountPolicy discountPolicy) {
         this.title = title;
         this.runningTime = runningTime;
         this.fee = fee;
-        this.defaultDiscountPolicy = defaultDiscountPolicy;
+        this.discountPolicy = discountPolicy;
     }
 
     public Money getFee() {
@@ -24,6 +27,10 @@ public class Movie {
     }
 
     public Money calculateMovieFee(Screening screening) {
-        return fee.minus(defaultDiscountPolicy.calculateDiscountAmount(screening));
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+    }
+
+    public void changeDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
     }
 }
